9f810fa2eff26eb3dcc8e06cad1dce873bbeda76
Absolu242 <wangleibniz01@gmail.com>
2022-09-20 10:10:28 +0100

    first commit

---
+++ src/styles/colors.ts
@@ -0,0 +1,31 @@
+
+
+export const colors = {
+    red1:"#EF2F6E",
+    red2:"#FA5087",
+    red3:"#FE729F",
+    red4:"#FF95B7",
+    green1:"#04B78A",
+    green2:"#1FD0A3",
+    green3:"#59DDBB",
+    green4:"#8DEAD2",
+    yellow1:"#F2A626",
+    yellow2:"#FFC800",
+    yellow3:"#FFD655",
+    yellow4:"#FFEAA7",
+    blue1:"#0080FF",
+    blue2:"#369AFE",
+    blue3:"#56AAFF",
+    blue4:"#8FC6FE",
+    blueberry1:"#4540BB",
+    blueberry2:"#6863FB",
+    blueberry3:"#9390F3",
+    blueberry4:"#B5B3FF",
+    text:{
+        main:'#242F57',
+        secondary:"#636E95",
+        icon:"#7C88B1",
+        tertiary:"#97A0C3",
+        white:"#ffffff"
+    }
+}
